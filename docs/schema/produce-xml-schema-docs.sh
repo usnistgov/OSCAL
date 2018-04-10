35bf7f72ddcdf8a9ce53cedbd36709fd55d58977
Wendell Piez <wapiez@wendellpiez.com>
2018-04-10 12:12:41 -0400

    More cleanup of shell script for intelligibility

--- docs/schema/produce-xml-schema-docs.sh
+++ docs/schema/produce-xml-schema-docs.sh
@@ -1,13 +1,8 @@
-# Invokes produce-schema-set.xpl to produce all three schema modules with injected docs
+# Invokes produce-schema-docs-md.xpl to produce markdown-formatted documentation of schema modules
 
 CALABASH=/home/wendell/xmlcalabash-1.1.20-98/xmlcalabash-1.1.20-98.jar
 
-# Calls to Calabash for each of 'catalog', 'profile' and 'declarations'
-CATALOG_DOCS="java -jar $CALABASH -i schema-docs=oscal-oscal.xml -o html-docs=__catalogXML.html -o markdown-docs=__catalogXML.md lib/produce-schema-module-docs.xpl module=catalog"
-PROFILE_DOCS="java -jar $CALABASH -i schema-docs=oscal-oscal.xml -o html-docs=__profileXML.html -o markdown-docs=__profileXML.md lib/produce-schema-module-docs.xpl module=profile"
-DECLARATIONS_DOCS="java -jar $CALABASH -i schema-docs=oscal-oscal.xml -o html-docs=__declarations-docs.html -o markdown-docs=__declarationsXML.md lib/produce-schema-module-docs.xpl module=declarations"
-
-# Alternatively, a single call to a pipeline that writes all three, while producing only the Markdown results not the HTML
+# We call an XProc pipeline that produces docs in Markdown for three modules at once and writes the results in the current directory
 ALL_MD_DOCS="java -jar $CALABASH -i source=oscal-oscal.xml lib/produce-schema-docs-md.xpl"
 
 # Now ...
@@ -18,6 +13,13 @@ echo Processing schema documentation ...
 echo $ALL_MD_DOCS
 $ALL_MD_DOCS
 echo PRODUCED .md files
+
+# Alternatively, for debugging they can be produced separately along with HTML versions, calling the subpipeline
+
+# Calls to Calabash for each of 'catalog', 'profile' and 'declarations'
+# CATALOG_DOCS="java -jar $CALABASH -i schema-docs=oscal-oscal.xml -o html-docs=__catalogXML.html -o markdown-docs=__catalogXML.md lib/produce-schema-module-docs.xpl module=catalog"
+# PROFILE_DOCS="java -jar $CALABASH -i schema-docs=oscal-oscal.xml -o html-docs=__profileXML.html -o markdown-docs=__profileXML.md lib/produce-schema-module-docs.xpl module=profile"
+# DECLARATIONS_DOCS="java -jar $CALABASH -i schema-docs=oscal-oscal.xml -o html-docs=__declarations-docs.html -o markdown-docs=__declarationsXML.md lib/produce-schema-module-docs.xpl module=declarations"
 
 # echo $CATALOG_DOCS
 # $CATALOG_DOCS
