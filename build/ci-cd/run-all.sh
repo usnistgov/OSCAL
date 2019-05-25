3c0ae77a4f3749f27e0e72480c30d7ab88a13f2c
Travis Howerton <howieavp@yahoo.com>
2019-05-25 19:27:20 -0400

    XML RoundTrip in Run All Tests

--- build/ci-cd/run-all.sh
+++ build/ci-cd/run-all.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-
+# determines the OSCAL directory path
 if [[ -z "$OSCALDIR" ]]; then
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
     source "$DIR/common-environment.sh"
 fi
 
+# sets the CI-CD directory
 CIDIR=$OSCALDIR/build/ci-cd
 
+# runs all of the scripts to test locally
 bash $CIDIR/validate-metaschema.sh
 bash $CIDIR/generate-schema.sh
+bash $CIDIR/validate-content.sh
 bash $CIDIR/generate-content-converters.sh
 bash $CIDIR/copy-and-convert-content.sh
+bash $CIDIR/roundTripXML.sh
