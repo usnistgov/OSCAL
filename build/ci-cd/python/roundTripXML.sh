65ffa273b5318142908fce91ae3294bdb17ced9b
Travis Howerton <howieavp@yahoo.com>
2019-05-25 13:24:36 -0400

    Added Exit Code Logic to XML Round Trip Comparison

--- build/ci-cd/python/roundTripXML.sh
+++ build/ci-cd/python/roundTripXML.sh
@@ -4,20 +4,45 @@
 source ../common-environment.sh
 
 ###################################################################################################################
-#   NIST 800-53 Rev4 Catalog conversions tests
+#   XML->JSON Roundtrip conversions tests
 ###################################################################################################################
+
+# default to test passed at zero
+exitcode=0
 
 # transformation from source XML to target XML
 java -jar saxon9he.jar -s:${OSCALDIR}/content/nist.gov/SP800-53/rev4/xml/NIST_SP-800-53_rev4_catalog.xml -xsl:${OSCALDIR}/json/convert/oscal-catalog-xml-to-json-converter.xsl -o:composedJSON.json
-echo "XML converted to JSON"
+# check the exit code for the conversion
+cmd_exitcode=$?
+if [ $cmd_exitcode -ne 0 ]; then
+    printf 'XML->JSON conversion failed for %s\n' "$file"
+    exitcode=1
+else
+    echo "XML converted to JSON"
+fi
 
 # transformation of JSON back to XML
 java -jar saxon9he.jar  -o:composedXML.xml -it:start -xsl:${OSCALDIR}/xml/convert/oscal-catalog-json-to-xml-converter.xsl json-file=${OSCALDIR}/build/ci-cd/python/composedJSON.json
-echo "JSON converted back to XML"
+# check the exit code for the conversion
+cmd_exitcode=$?
+if [ $cmd_exitcode -ne 0 ]; then
+    printf 'JSON->XML conversion failed for %s\n' "$file"
+    exitcode=1
+else
+    printf "JSON converted back to XML. \n"
+fi
+
 
 # compare the XML files to see if there is data loss
 echo "Checking XML->JSON->XML conversion for the NIST 800-53 rev4 catalog"
 python xmlComparison.py ${OSCALDIR}/content/nist.gov/SP800-53/rev4/xml/NIST_SP-800-53_rev4_catalog.xml ${OSCALDIR}/build/ci-cd/python/composedXML.xml
+cmd_exitcode=$?
+if [ $cmd_exitcode -ne 0 ]; then
+    printf 'XML roundtrip comparison failed.\n'
+    exitcode=1
+else
+    echo "XML round trip comparison was successful.\n"
+fi
 
 #use the XMLDIFF tool to see differences
 echo ${OSCALDIR}/content/nist.gov/SP800-53/rev4/xml/NIST_SP-800-53_rev4_catalog.xml
@@ -28,3 +53,10 @@ echo ${OSCALDIR}/build/ci-cd/python/composedXML.xml
 
 #validate JSON schemas
 ajv validate -s ${OSCALDIR}/json/schema/oscal-catalog-schema.json -d ${OSCALDIR}/content/nist.gov/SP800-53/rev4/json/NIST_SP-800-53_rev4_catalog.json --extend-refs=true --verbose
+cmd_exitcode=$?
+if [ $cmd_exitcode -ne 0 ]; then
+    printf 'Comparison of the converted JSON file to the original failed.\n'
+    exitcode=1
+else
+    echo "Comparison of the converted JSON file to the original was successful.\n"
+fi
