344d008bb9bcf3b56e3d1c6eaef51e48a93e9c8e
David Waltermire <david.waltermire@nist.gov>
2019-05-29 11:41:58 -0400

    Validate metaschema using schematron (#382)
    * Added support for Schematron validation of metaschema.
    * Added documentation on schematron environment setup
    * Fixed result messages
    * Added SVRL stylesheet to render plaintext results.

--- build/ci-cd/validate-metaschema.sh
+++ build/ci-cd/validate-metaschema.sh
@@ -5,6 +5,26 @@ if [[ -z "$OSCALDIR" ]]; then
     source "$DIR/common-environment.sh"
 fi
 
+source $OSCALDIR/build/ci-cd/schematron-init.sh
+
+if [[ -z "$1" ]]; then
+  working_dir=$OSCALDIR
+else
+  working_dir=$1
+fi
+
+# compile the schematron
+metaschema_lib="$OSCALDIR/build/metaschema/lib"
+schematron="$metaschema_lib/metaschema-check.sch"
+compiled_schematron="${schematron}-compiled.xsl"
+
+build_schematron "$schematron" "$compiled_schematron"
+cmd_exitcode=$?
+if [ $cmd_exitcode -ne 0 ]; then
+  printf "Compilation of Schematron '%s' failed" "$schematron"
+  exit 1
+fi
+
 exitcode=0
 shopt -s nullglob
 shopt -s globstar
@@ -23,17 +43,25 @@ while IFS="" read -r path || [[ -n "$path" ]]; do
     IFS= # disable word splitting    
     for file in $files_to_process
     do
-      printf 'Validating: %s\n' "$file"
-      xmllint --noout --schema "$OSCALDIR/build/metaschema/lib/metaschema.xsd" "$file"
+      printf 'Validating: '%s'\n' "$file"
+      xmllint --noout --schema "$metaschema_lib/metaschema.xsd" "$file"
       cmd_exitcode=$?
       if [ $cmd_exitcode -ne 0 ]; then
         printf 'XML schema validation failed for %s\n' "$file"
         exitcode=1
+      else
+        svrl_result="$working_dir/svrl/${file/$OSCALDIR\/src\//}.svrl"
+        svrl_result_dir=${svrl_result%/*}
+        mkdir -p "$svrl_result_dir"
+        validate_with_schematron "$compiled_schematron" "$file" "$svrl_result"
       fi
     done
   fi
 done < $OSCALDIR/build/ci-cd/config/metaschema
 shopt -u nullglob
 shopt -u globstar
+
+# cleanup compiled schematron
+rm -f "$compiled_schematron"
 
 exit $exitcode
