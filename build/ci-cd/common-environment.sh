8945878f0130fd32480c700ef20c7fdc471d9c47
david-waltermire-nist <david.waltermire@nist.gov>
2019-05-31 17:20:55 -0400

    Added conditional logic for color codes

--- build/ci-cd/common-environment.sh
+++ build/ci-cd/common-environment.sh
@@ -6,9 +6,28 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 OSCALDIR=$(cd "$DIR/../.."; pwd)
 
-#setup print colors
-P_ERROR=$'\e[1;31m'
-P_OK=$'\e[1;32m'
-P_WARN=$'\e[1;33m'
-P_INFO=$'\e[1m'
-P_END=$'\e[0m'
+## Setup color codes
+# check if stdout is a terminal...
+colorize=0
+if [ -t 1 ]; then
+  # does the terminal support colors?
+  ncolors=$(tput colors)
+  if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
+    colorize=1
+  fi
+fi
+
+if [ $colorize -eq 1 ]; then
+    #setup print colors
+    P_ERROR=$(tput setaf 1)$(tput bold)
+    P_OK=$(tput setaf 2)$(tput bold)
+    P_WARN=$(tput setaf 3)$(tput bold)
+    P_INFO=$(tput bold)
+    P_END=$(tput sgr0)
+else
+    P_ERROR=""
+    P_OK=""
+    P_WARN=""
+    P_INFO=""
+    P_END=""
+fi
