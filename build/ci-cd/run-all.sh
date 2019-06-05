27bcab95f478682a9e89f6b3a350d49b909e600f
David Waltermire <david.waltermire@nist.gov>
2019-06-05 13:00:58 -0400

    Document Schema File Naming Conventions (#391)
    - Added file naming conventions for XSL conversion templates
    - Renamed schema files to follow the naming guidance
    - Reorganized metaschema docuemntation
    - Refactored scripts to produce files using correct names
    - Fixed broken links in repo and website
    - Fixed spelling errors and broken links
    - Added markdown-link-checker to CI process to check PRs for broken links
    - Added colorization to many build messages
    - Added support for JSON min and YAML content generation from JSON files (using prettyprint)

--- build/ci-cd/run-all.sh
+++ build/ci-cd/run-all.sh
@@ -8,6 +8,7 @@ fi
 # sets the CI-CD directory
 CIDIR=$OSCALDIR/build/ci-cd
 
+# runs all of the scripts to test locally
 bash $CIDIR/validate-metaschema.sh || ("Failed to validate Metaschema" && exit 1)
 bash $CIDIR/generate-schema.sh || ("Failed to generate schema files" && exit 2)
 bash $CIDIR/validate-content.sh || ("Failed to validate content" && exit 3)
