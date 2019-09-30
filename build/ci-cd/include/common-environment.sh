052c2bbdc42322c67c138c04a2bf675e7c921484
david-waltermire-nist <david.waltermire@nist.gov>
2019-09-30 11:15:22 -0400

    hardcode ascii color codes

--- build/ci-cd/include/common-environment.sh
+++ build/ci-cd/include/common-environment.sh
@@ -20,11 +20,16 @@ fi
 
 if [ $colorize -eq 1 ]; then
     #setup print colors
-    P_ERROR="$(tput setaf 1)$(tput bold)"
-    P_OK="$(tput setaf 2)$(tput bold)"
-    P_WARN="$(tput setaf 3)$(tput bold)"
-    P_INFO="$(tput bold)"
-    P_END="$(tput sgr0)"
+#    P_ERROR="$(tput setaf 1)$(tput bold)"
+#    P_OK="$(tput setaf 2)$(tput bold)"
+#    P_WARN="$(tput setaf 3)$(tput bold)"
+#    P_INFO="$(tput bold)"
+#    P_END="$(tput sgr0)"
+    P_ERROR="\u001b[31m\u001b[1m"
+    P_OK="\u001b[32m\u001b[1m"
+    P_WARN="\u001b[33m\u001b[1m"
+    P_INFO="\u001b[37m\u001b[1m" # white bold
+    P_END="\u001b[0m" # reset
 else
     P_ERROR=""
     P_OK=""
