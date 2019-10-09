d1fc244d48ab1fb598c6db05f5cfbd77f0cf01c1
david-waltermire-nist <david.waltermire@nist.gov>
2019-10-09 10:04:10 -0400

    Added support for single metaschema doc generation

--- build/ci-cd/generate-model-documentation.sh
+++ build/ci-cd/generate-model-documentation.sh
@@ -13,7 +13,7 @@ HELP=false
 
 usage() {                                      # Function: Print a help message.
   cat << EOF
-Usage: $0 [options]
+Usage: $0 [options] [metaschema paths]
 
 -h, --help                        Display help
 -w DIR, --working-dir DIR         Generate artifacts in DIR
@@ -64,6 +64,34 @@ if [ "$VERBOSE" = "true" ]; then
   echo -e "${P_INFO}Using working directory:${P_END} ${WORKING_DIR}"
 fi
 
+declare -a paths
+declare -a formats
+if [ "$#" -ne 0 ]; then
+  paths=("$@")
+  for i in "${!paths[@]}"; do
+    formats[$i]="xml,json"
+  done
+else
+  while IFS="|" read path gen_schema gen_converter gen_docs || [[ -n "$path" ]]; do
+    [[ "$path" =~ ^[[:space:]]*# ]] && continue
+    # remove leading space
+    path="${path##+([[:space:]])}"
+
+    ([ -z "$path" ] || [ -z "$gen_docs" ]) && continue;
+
+    path_absolute="$OSCALDIR"/"$path"
+
+    IFS_OLD=$IFS
+    IFS= # disable word splitting
+    for metaschema in $path_absolute
+    do
+      paths+=("$metaschema")
+      formats+=("$gen_converter")
+    done
+    IFS=$IFS_OLD
+  done < "$OSCALDIR/build/ci-cd/config/metaschema"
+fi
+
 # the stylesheet used to generate the documentation
 stylesheet="$OSCALDIR/build/metaschema/xml/produce-and-run-either-documentor.xsl"
 
@@ -75,88 +103,77 @@ schema_doc_dir="${WORKING_DIR}/docs/layouts/partials/generated"
 mkdir -p "$schema_doc_dir" # ensure this directory exists
 
 exitcode=0
-shopt -s nullglob
-shopt -s globstar
-while IFS="|" read path gen_schema gen_converter gen_docs || [[ -n "$path" ]]; do
-  shopt -s extglob
-  [[ "$path" =~ ^[[:space:]]*# ]] && continue
-  # remove leading space
-  path="${path##+([[:space:]])}"
-  # remove trailing space
-  gen_docs="${gen_docs%%+([[:space:]])}"
-  shopt -u extglob
+for i in "${!paths[@]}"; do
+  metaschema="${paths[$i]}"
+  gen_docs="${formats[$i]}"
 
-  ([ -z "$path" ] || [ -z "$gen_docs" ]) && continue;
+  filename=$(basename -- "$metaschema")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  base="${filename/_metaschema/}"
+  metaschema_relative=$(realpath --relative-to="$WORKING_DIR" "$metaschema")
+  converter="$WORKING_DIR/json/convert/${base}_xml-to-json-converter.xsl"
 
-  files_to_process="$OSCALDIR"/"$path"
+  # Make xslt paths relative to current directory
+  metaschema_path=$(realpath --relative-to="$PWD" "$metaschema")
+  stylesheet_path=$(realpath --relative-to="$PWD" "$stylesheet")
+  output_path=$(realpath --relative-to="$PWD" "$schema_doc_dir")
+  # Make converter path relative to the stylesheet
+  stylesheet_dir=$(dirname "$stylesheet")
+  converter_path=$(realpath --relative-to="$stylesheet_dir" "$converter")
 
-  IFS= # disable word splitting
-  for metaschema in $files_to_process
-  do
-    filename=$(basename -- "$metaschema")
-    extension="${filename##*.}"
-    filename="${filename%.*}"
-    base="${filename/_metaschema/}"
-    metaschema_relative=$(realpath --relative-to="$WORKING_DIR" "$metaschema")
-    converter="$WORKING_DIR/json/convert/${base}_xml-to-json-converter.xsl"
+  #split on commas
+  IFS_OLD=$IFS
+  IFS=, #read -a formats <<< "$gen_schema"
 
-    # Make xslt paths relative to current directory
-    metaschema_path=$(realpath --relative-to="$PWD" "$metaschema")
-    stylesheet_path=$(realpath --relative-to="$PWD" "$stylesheet")
-    output_path=$(realpath --relative-to="$PWD" "$schema_doc_dir")
-    # Make converter path relative to the stylesheet
-    stylesheet_dir=$(dirname "$stylesheet")
-    converter_path=$(realpath --relative-to="$stylesheet_dir" "$converter")
+  for format in ${gen_docs}; do
+    if [ -z "$format" ]; then
+      # skip blanks
+      continue;
+    fi
 
-    #split on commas
-    IFS=, read -a formats <<< "$gen_docs"
-    for format in "${formats[@]}"; do
-      if [ -z "$format" ]; then
-        # skip blanks
-        continue;
-      fi
+    # Run the XSL template for the format
+    case $format in
+    xml)
+      # determine web location of schema
+      schema_url="${github_url}/xml/schema/${base}_schema.xsd"
+      ;;
+    json)
+      # determine web location of schema
+      schema_url="${github_url}/json/schema/${base}_schema.json"
+      ;;
+    *)
+      echo -e "${P_WARN}Generating documentation for '${format^^}' is unsupported for '${P_END}${metaschema_relative}${P_WARN}'.${P_END}"
+      continue;
+      ;;
+    esac
 
-      # Run the XSL template for the format
-      case $format in
-      xml)
-        # determine web location of schema
-        schema_url="${github_url}/xml/schema/${base}_schema.xsd"
-        ;;
-      json)
-        # determine web location of schema
-        schema_url="${github_url}/json/schema/${base}_schema.json"
-        ;;
-      *)
-        echo -e "${P_WARN}Generating documentation for '${format^^}' is unsupported for '${P_END}${metaschema_relative}${P_WARN}'.${P_END}"
-        continue;
-        ;;
-      esac
-
+    if [ "$VERBOSE" = "true" ]; then
+      echo -e "  ${P_INFO}Generating ${format^^} model documentation for metaschema '${P_END}${metaschema_relative}${P_INFO}'.${P_END}"
+    fi
+    result=$(xsl_transform "$stylesheet_path" "$metaschema_path" "" \
+      "target-format=${format}" \
+      "example-converter-xslt-path=${converter_path}" \
+      "output-path=${output_path}" \
+      "schema-path=${schema_url}" 2>&1)
+    cmd_exitcode=$?
+    if [ $cmd_exitcode -ne 0 ]; then
+      echo -e "${P_ERROR}Generating ${format^^} model documentation failed for '${P_END}${metaschema_relative}${P_ERROR}'.${P_END}"
+      echo -e "${P_ERROR}${result}${P_END}"
+      exitcode=1
+      continue
+    else
       if [ "$VERBOSE" = "true" ]; then
-        echo -e "  ${P_INFO}Generating ${format^^} model documentation for metaschema '${P_END}${metaschema_relative}${P_INFO}'.${P_END}"
+        echo -e "${P_INFO}${result}${P_END}"
       fi
-      result=$(xsl_transform "$stylesheet_path" "$metaschema_path" "" \
-        "target-format=${format}" \
-        "example-converter-xslt-path=${converter_path}" \
-        "output-path=${output_path}" \
-        "schema-path=${schema_url}" 2>&1)
-      cmd_exitcode=$?
-      if [ $cmd_exitcode -ne 0 ]; then
-        echo -e "${P_ERROR}Generating ${format^^} model documentation failed for '${P_END}${metaschema_relative}${P_ERROR}'.${P_END}"
-        echo -e "${P_ERROR}${result}${P_END}"
-        exitcode=1
-        continue
-      else
-        model="${base/oscal_/}"
-        touch -c "$OSCALDIR/docs/content/documentation/schema/$model/${format}-model-map.md"
-        touch -c "$OSCALDIR/docs/content/documentation/schema/$model/${format}-schema.md"
-        echo -e "${P_OK}Generated ${format^^} model documentation for '${P_END}${metaschema_relative}${P_OK}'.${P_END}"
-      fi
-    done
-  done
-done < $OSCALDIR/build/ci-cd/config/metaschema
 
-shopt -u nullglob
-shopt -u globstar
+      model="${base/oscal_/}"
+      touch -c "$OSCALDIR/docs/content/documentation/schema/$model/${format}-model-map.md"
+      touch -c "$OSCALDIR/docs/content/documentation/schema/$model/${format}-schema.md"
+      echo -e "${P_OK}Generated ${format^^} model documentation for '${P_END}${metaschema_relative}${P_OK}'.${P_END}"
+    fi
+  done
+  IFS=$IFS_OLD
+done
 
 exit $exitcode
