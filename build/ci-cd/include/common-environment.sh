366f71470460eb9be0565fa1a717d1aebfd58262
anweiss <2326106+anweiss@users.noreply.github.com>
2019-12-12 14:38:07 -0500

    add Docker resources and update build scripts

--- build/ci-cd/include/common-environment.sh
+++ build/ci-cd/include/common-environment.sh
@@ -3,8 +3,11 @@
 # Setup script environment
 
 # Get location of this script and set the OSCAL directory as a relative path
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-OSCALDIR=$(cd "$DIR/../../.."; pwd)
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+OSCALDIR=$(
+  cd "$DIR/../../.."
+  pwd
+)
 
 ## Setup color codes
 # check if stdout is a terminal...
@@ -13,27 +16,27 @@ colorize=0
 if [ -t 1 ]; then
   # does the terminal support colors?
   ncolors=$(tput colors)
-  if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
+  if [ -n "$ncolors" ] && [ $ncolors -ge 8 ] && [[ "$OSTYPE" != "darwin"* ]]; then
     colorize=1
   fi
 fi
 
 if [ $colorize -eq 1 ]; then
-    #setup print colors
-#    P_ERROR="$(tput setaf 1)$(tput bold)"
-#    P_OK="$(tput setaf 2)$(tput bold)"
-#    P_WARN="$(tput setaf 3)$(tput bold)"
-#    P_INFO="$(tput bold)"
-#    P_END="$(tput sgr0)"
-    P_ERROR="\u001b[31m\u001b[1m"
-    P_OK="\u001b[32m\u001b[1m"
-    P_WARN="\u001b[33m\u001b[1m"
-    P_INFO="\u001b[37m\u001b[1m" # white bold
-    P_END="\u001b[0m" # reset
+  #setup print colors
+  #    P_ERROR="$(tput setaf 1)$(tput bold)"
+  #    P_OK="$(tput setaf 2)$(tput bold)"
+  #    P_WARN="$(tput setaf 3)$(tput bold)"
+  #    P_INFO="$(tput bold)"
+  #    P_END="$(tput sgr0)"
+  P_ERROR="\u001b[31m\u001b[1m"
+  P_OK="\u001b[32m\u001b[1m"
+  P_WARN="\u001b[33m\u001b[1m"
+  P_INFO="\u001b[37m\u001b[1m" # white bold
+  P_END="\u001b[0m"            # reset
 else
-    P_ERROR=""
-    P_OK=""
-    P_WARN=""
-    P_INFO=""
-    P_END=""
+  P_ERROR=""
+  P_OK=""
+  P_WARN=""
+  P_INFO=""
+  P_END=""
 fi
