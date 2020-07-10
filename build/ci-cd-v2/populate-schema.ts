91b71fb627487f0c04e7a08618ecb6095fa2d4c2
Adougla2 <braddoug76@yahoo.com>
2020-07-10 16:44:11 -0400

    Initial commit of typescript code supporting schema files generation

---
+++ build/ci-cd-v2/populate-schema.ts
@@ -0,0 +1,40 @@
+import {spawnSync} from 'child_process';
+import fs from 'fs';
+
+export const populateSchema = (oscalRootDirectory: string, format: string, base: string) => {
+  try {
+    const targetFiles = fs.readdirSync(`${oscalRootDirectory}/${format}/schema`)
+    const sourceFiles = fs.readdirSync(`${oscalRootDirectory}/src/metaschema`)
+
+    const getXMLSourceFiles = sourceFiles.filter(file => file.endsWith('xml'));
+
+    console.log('TARGET_FILES', targetFiles);
+      
+    const source = getXMLSourceFiles.find(file => file.startsWith(base));
+    const target = targetFiles.find(file => file.startsWith(base));
+
+    console.log('SOURCE_FILE', source);
+    console.log('TARGET_FILE', target);
+
+    if (source && target) {
+      // ['--format', 'source', 'target', '--validate']
+      const scriptArguments = [
+        `--${format}`,
+        `${oscalRootDirectory}/src/metaschema/${source}`,
+        `${oscalRootDirectory}/${format}/schema/${target}`,
+        '--validate'
+      ]
+
+      console.log('scriptArguments:', scriptArguments)
+
+      // spawnSync('command', ['arg1', 'arg2', 'arg3'], spawn_option)
+      const result = spawnSync(`${oscalRootDirectory}/build/metaschema/scripts/generate-schema.sh`, scriptArguments, {shell: true, stdio: 'inherit'});
+      if (!result || result.status === 1) {
+        process.exit(1);
+      }
+    } 
+  } catch (error) {
+      console.error(error);
+      process.exit(1);
+  }
+}
