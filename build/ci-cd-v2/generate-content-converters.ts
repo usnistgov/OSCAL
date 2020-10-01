0336faf3f1d0a699473ba90b9c194c9b19ace0c3
Adougla2 <braddoug76@yahoo.com>
2020-10-01 02:44:27 -0400

    Renamed scripts to the original filenames

--- build/ci-cd-v2/generate-content-converters.ts
+++ build/ci-cd-v2/generate-content-converters.ts
@@ -13,77 +13,33 @@ process.cwd();
 import fs from 'fs';
 import path from 'path';
 import {argv} from 'yargs';
-import {populateConvert} from './populate-convert';
-
-// Set console log colors
-const P_INFO = '\x1b[37m\x1b[1m'; // White
-const P_OK = '\x1b[32m\x1b[1m'; // Green
-const P_PATH = '\x1b[50m'; // Faded white
-const P_WARN = '\x1b[33m\x1b[1m'; // Yellow
-const P_ERROR = '\x1b[31m\x1b[1m'; // Red
-const P_END = '\x1b[0m'; // Reset
+import {populateConvert} from './utils/populate-convert';
+import {colorCodes, setupCliInputs, createWorkingDir} from './utils/init'
 
 // Set the root project root directory
 let oscalRootDirectory: any = path.join(__dirname, '../..');
 
-// Create a message log to be used with stdout and stderr
-const usage = () => `
-  Usage: node ${argv.$0} [options] [metaschema paths]
-
-  -h, --help                        Display help
-  -w DIR, --working-dir DIR         Generate artifacts in DIR
-  -v                                Provide verbose output
-`;
-
-// Get the options parsed through command line interface using argv from the yargs module
-const optionsFromCLI = Object.keys(argv);
-// Define an array of acceptable options, which optionsFromCLI will be compared with for validation
-const options = ['_', '$0', 'v', 'w', 'working-dir', 'workingDir', 'h', 'help'];
-
-// Compare options parsed from the command line with the acceptable options defined above
-const optionDifference = optionsFromCLI.find((option) => !options.includes(option));
-
-// If an unacceptable option or working directory option was parsed without a value, terminate the process as an error
-// Note an option parsed without a value will assume a value of 'true'
-if (optionDifference || argv['w'] === true || argv['working-dir'] === true) {
-  console.log('Failed parsing options.');
-  process.stderr.write(usage());
-  process.exit(1);
-}
-
-// If the options includes the help option, terminate the process
-if (optionsFromCLI.includes('h') || optionsFromCLI.includes('help')) {
-  process.stdout.write(usage());
-  process.exit(0);
-}
+// initialize the setup script
+setupCliInputs(oscalRootDirectory, argv);
 
 try {
   let workingDirectory: any;
 
   // If working directory is defined and has a value
   if (argv['w'] || argv['working-dir']) {
-    // Assign the directory to a variable
-    const parsedWorkingDir = argv['w'] || argv['working-dir'];
-    // Create the directory defined from the command line interface
-    fs.mkdirSync(`${parsedWorkingDir}`, {recursive: true});
     // Assign the relative path of the working directory to a variable
-    workingDirectory = `${process.cwd()}/${parsedWorkingDir}`;
+    workingDirectory = createWorkingDir(argv, fs)
   }
 
-  // Get all files in the metaschema directory
-  const sourceFiles = fs.readdirSync(`${oscalRootDirectory}/src/metaschema`);
-  // // Get only files with xml extensions in the metaschema directory
-  const getXMLSourceFiles = sourceFiles.filter((file) => file.endsWith('xml'));
-
   console.log('');
-  console.log(`${P_INFO}Generating XML <-> JSON Content Converters${P_END}`);
-  console.log(`${P_INFO}==========================================${P_END}`);
+  console.log(`${colorCodes.P_INFO}Generating XML <-> JSON Content Converters${colorCodes.P_END}`);
+  console.log(`${colorCodes.P_INFO}==========================================${colorCodes.P_END}`);
 
   // If '-v' option is parsed, the value will default to true. Assign the value to the 'verbose' variable
   const verbose: any = argv.v;
 
   if (verbose === true) {
-    console.log(`${P_INFO}Using working directory:${P_END} ${P_PATH}${oscalRootDirectory}${P_END}`);
+    console.log(`${colorCodes.P_INFO}Using working directory:${colorCodes.P_END} ${colorCodes.P_PATH}${oscalRootDirectory}${colorCodes.P_END}`);
   }
 
   // Get an array of arguments parsed from command line interface (the args should be the configs)
@@ -105,13 +61,15 @@ try {
 
   metaSchemaArray.forEach((schema) => {
     const schemaPathWithFormat = schema.split('|');
-    // Each Metaschema path is read from the config
-    const metaSchemaRelativePath = schemaPathWithFormat[0]; // src/metaschema/file1.xml
+    // Each Metaschema path read from the config
+    const metaSchemaFullPath = schemaPathWithFormat[0];
     // Split each metaschema path into an array
-    const metaSchemaRelativePathArray = metaSchemaRelativePath.split('/');
-    const lengthOfPathArray = metaSchemaRelativePathArray.length
+    const metaSchemaFullPathArray = metaSchemaFullPath.split('/');
+    const lengthOfPathArray = metaSchemaFullPathArray.length
     // Get metaschema file name
-    const metaschemaFile = metaSchemaRelativePathArray[lengthOfPathArray - 1];
+    const metaschemaFile = metaSchemaFullPathArray[lengthOfPathArray - 1];
+    // Get the root directory of the metaschema
+    const metaschemaFileRootDir = metaSchemaFullPathArray.slice(0, lengthOfPathArray - 1).join('/');
     // Get the base of each metaschema file from the metaschema file name
     const base = metaschemaFile.replace(/(_metaschema).*/g, '');
 
@@ -139,7 +97,7 @@ try {
         const validFormats = ['xml', 'json'];
 
         if (!validFormats.includes(format)) {
-          return console.log(`${P_ERROR}Generating converter from '${P_END}${sourceFormat.toUpperCase()}${P_ERROR}' to '${P_END}${targetFormat.toUpperCase()}${P_ERROR}' is unsupported for '${P_END}${P_PATH}${metaSchemaRelativePath}${P_END}${P_ERROR}'${P_END}.`);
+          return console.log(`${colorCodes.P_ERROR}Generating converter from '${colorCodes.P_END}${sourceFormat.toUpperCase()}${colorCodes.P_ERROR}' to '${colorCodes.P_END}${targetFormat.toUpperCase()}${colorCodes.P_ERROR}' is unsupported for '${colorCodes.P_END}${colorCodes.P_PATH}${metaSchemaFullPath}${colorCodes.P_END}${colorCodes.P_ERROR}'${colorCodes.P_END}.`);
         }
 
         /*
@@ -154,34 +112,39 @@ try {
           fs.mkdirSync(`${oscalRootDirectory}/${format}/convert`, {recursive: true});
         }
 
-        // Assign the convert file path to a variable for reusability purpose
+        // Assign the schema file path to a variable for reusability purpose
         const convertFile = `${format}/convert/${base}_${sourceFormat}-to-${targetFormat}-converter.xsl`
 
-        // Check if the metaschama defined in the config exists as a file in the metaschema directory
+        // Get all files in the metaschema directory
+        const sourceFiles = fs.readdirSync(`${oscalRootDirectory}/${metaschemaFileRootDir}`);
+        // Get only files with xml extensions in the metaschema directory
+        const getXMLSourceFiles = sourceFiles.filter((file) => file.endsWith('xml'));
+
+        // Check if the metaschame defined in the config exists as a file in the metaschema directory
         const metaSchemaExist = getXMLSourceFiles.includes(metaschemaFile);
 
         if (metaSchemaExist) {
           // If working directory is defined
           if (workingDirectory) {
             /*
-              Create the convert files in the respective XML and JSON directories, relative to the defined working directory.
+              Create the schema files in the respective XML and JSON directories, relative to the defined working directory.
               The target files (XML and JSON schemas) will be created here
             */
             fs.openSync(`${workingDirectory}/${convertFile}`, 'w');
-            // populate the convert files
-            populateConvert(oscalRootDirectory, workingDirectory, format, metaschemaFile, convertFile, verbose);
+            // populate the schema files
+            populateConvert(oscalRootDirectory, workingDirectory, metaschemaFileRootDir, format, metaschemaFile, convertFile, verbose);
           } else { // If working directory is not defined
             /*
-              Create the convert files in the respective XML and JSON directories, using project root directory as default.
+              Create the schema files in the respective XML and JSON directories, using project root directory as default.
               The target files will be created in the project root directory
             */
             fs.openSync(`${oscalRootDirectory}/${convertFile}`, 'w');
             // populate the schema files
-            populateConvert(oscalRootDirectory, '', format, metaschemaFile, convertFile, verbose);
+            populateConvert(oscalRootDirectory, '', metaschemaFileRootDir, format, metaschemaFile, convertFile, verbose);
           }
         } else { // If metaschema does not exist
-          console.log(`${P_ERROR}Metaschema does not exist: ${P_END}${P_PATH}${oscalRootDirectory}/${metaSchemaRelativePath}${P_END}`);
-          console.log(`${P_ERROR}Generation of ${sourceFormat.toUpperCase()} to ${targetFormat.toUpperCase()} converter failed for '${P_END}${P_PATH}${metaSchemaRelativePath}${P_END}${P_ERROR}'${P_END}.`);
+          console.log(`${colorCodes.P_ERROR}Metaschema does not exist: ${colorCodes.P_END}${colorCodes.P_PATH}${oscalRootDirectory}/${metaSchemaFullPath}${colorCodes.P_END}`);
+          console.log(`${colorCodes.P_ERROR}Generation of ${sourceFormat.toUpperCase()} to ${targetFormat.toUpperCase()} converter failed for '${colorCodes.P_END}${colorCodes.P_PATH}${metaSchemaFullPath}${colorCodes.P_END}${colorCodes.P_ERROR}'${colorCodes.P_END}.`);
         }
       });
     }
--- build/ci-cd-v2/generate-schema.ts
+++ build/ci-cd-v2/generate-schema.ts
@@ -1,11 +1,11 @@
 /*
-  Allow JS file to run from anywhere by targeting the script relative path. 
+  Allow JS file to run from anywhere by targeting the script relative path.
   Files and Directories are created using the 'file location' as the reference point
 */
 //process.chdir(__dirname);
 
 /*
-  Allow JS file to run from anywhere by targeting the script relative path. 
+  Allow JS file to run from anywhere by targeting the script relative path.
   Files and Directories are created using the 'current working directory' as the reference point
 */
 process.cwd();
@@ -13,61 +13,22 @@ process.cwd();
 import fs from 'fs';
 import path from 'path';
 import {argv} from 'yargs';
-import {populateSchema} from './populate-schema';
-
-// set console log colors
-const P_INFO = '\x1b[37m\x1b[1m'; // White
-const P_OK = '\x1b[32m\x1b[1m'; // Green
-const P_PATH = '\x1b[50m'; // Faded white
-const P_WARN = '\x1b[33m\x1b[1m'; // Yellow
-const P_ERROR = '\x1b[31m\x1b[1m'; // Red
-const P_END = '\x1b[0m'; // Reset
-
-// Get the options parsed through command line interface using argv from the yargs module
-const optionsFromCLI = Object.keys(argv);
-// Define an array of acceptable options, which optionsFromCLI will be compared with for validation
-const options = ['_', '$0', 'v', 'w', 'working-dir', 'workingDir', 'h', 'help'];
+import {populateSchema} from './utils/populate-schema';
+import {colorCodes, setupCliInputs, createWorkingDir} from './utils/init'
 
 // Set the root project root directory
 let oscalRootDirectory: any = path.join(__dirname, '../..');
 
-// Create a message log to be used with stdout and stderr
-const usage = () => `
-  Usage: node ${argv.$0} [options] [metaschema paths]
-
-  -h, --help                        Display help
-  -w DIR, --working-dir DIR         Generate artifacts in DIR  (default: ${oscalRootDirectory})
-  -v                                Provide verbose output
-`;
-
-// Compare options parsed from command line with the acceptable options defined above
-const optionDifference = optionsFromCLI.find(option => !options.includes(option));
-
-// If there is an un-acceptable option or working directory option was parsed without a value, terminate the process as error
-// Note an option parsed without a value will assume a value of 'true'
-if (optionDifference || argv['w'] === true || argv['working-dir'] === true) {
-  console.log('Failed parsing options.');
-  process.stdout.write(usage());
-  process.exit(1);
-}
-
-// If the options includes the help option, terminate the process
-if (optionsFromCLI.includes('h') || optionsFromCLI.includes('help')) {
-  process.stderr.write(usage());
-  process.exit(0);
-}
+// initialize the setup script
+setupCliInputs(oscalRootDirectory, argv);
 
 try {
   let workingDirectory: any;
 
   // If working directory is defined and has a value
   if (argv['w'] || argv['working-dir']) { // any directory name that is specified
-    // Assign the directory to a variable
-    const parsedWorkingDir = argv['w'] || argv['working-dir'];
-    // Create the directory defined from the command line interface
-    fs.mkdirSync(`${parsedWorkingDir}`,{recursive: true});
     // Assign the relative path of the working directory to a variable
-    workingDirectory = `${process.cwd()}/${parsedWorkingDir}`;
+    workingDirectory = createWorkingDir(argv, fs)
    }
 
   // Get all files in the metaschema directory
@@ -76,16 +37,16 @@ try {
   const getXMLSourceFiles = sourceFiles.filter(file => file.endsWith('xml'));
 
   console.log('')
-  console.log(`${P_INFO}Generating XML and JSON Schema${P_END}`)
-  console.log(`${P_INFO}==============================${P_END}`)
+  console.log(`${colorCodes.P_INFO}Generating XML and JSON Schema${colorCodes.P_END}`)
+  console.log(`${colorCodes.P_INFO}==============================${colorCodes.P_END}`)
 
   // If '-v' option is parsed, the value will default to true. Assign the value to the 'verbose' variable
   const verbose: any = argv.v;
 
   if (verbose === true) {
-    console.log(`${P_INFO}Using working directory:${P_END} ${P_PATH}${oscalRootDirectory}${P_END}`);
+    console.log(`${colorCodes.P_INFO}Using working directory:${colorCodes.P_END} ${colorCodes.P_PATH}${oscalRootDirectory}${colorCodes.P_END}`);
   }
-  
+
   // Get an array of arguments parsed from command line interface (the args should be the configs)
   let metaSchemaArray: string[];
   const readConfigFilesFromCLI = argv._;
@@ -105,18 +66,21 @@ try {
 
   metaSchemaArray.forEach((schema) => {
     const schemaPathWithFormat = schema.split('|');
-    // Read the path for Each Metaschema from the config
-    const metaSchemaPath = schemaPathWithFormat[0];
+    // Each Metaschema path read from the config
+    const metaSchemaFullPath = schemaPathWithFormat[0];
     // Split each metaschema path into an array
-    const metaSchemaPathArray = metaSchemaPath.split('/');
+    const metaSchemaFullPathArray = metaSchemaFullPath.split('/');
+    const lengthOfPathArray = metaSchemaFullPathArray.length
     // Get metaschema file name
-    const metaschemaFile = metaSchemaPathArray[metaSchemaPathArray.length - 1];
+    const metaschemaFile = metaSchemaFullPathArray[lengthOfPathArray - 1];
+    // Get the root directory of the metaschema
+    const metaschemaFileRootDir = metaSchemaFullPathArray.slice(0, lengthOfPathArray - 1).join('/');
     // Get the base of each metaschema file from the metaschema file name
     const base = metaschemaFile.replace(/(_metaschema).*/g, '');
 
     // The second element after splitting at the '|' is the schema formats
     const formatOptions = schemaPathWithFormat[1];
-   
+
     if (formatOptions) {
       // Split each formats at the comma sign to create an array of format to be used for schema generation in each iteration
       const formatArray = formatOptions.split(',');
@@ -126,7 +90,7 @@ try {
         const validFormats = ['xml', 'json'];
 
         if (!validFormats.includes(format)) {
-          return console.log(`${P_WARN}Unsupported schema format '${format.toUpperCase()}' schema for '${P_PATH}${metaSchemaPath}${P_END}${P_WARN}'${P_END}.`);
+          return console.log(`${colorCodes.P_WARN}Unsupported schema format '${format.toUpperCase()}' schema for '${colorCodes.P_PATH}${metaSchemaFullPath}${colorCodes.P_END}${colorCodes.P_WARN}'${colorCodes.P_END}.`);
         }
 
         let fileExtension = format;
@@ -135,9 +99,9 @@ try {
           fileExtension = 'xsd';
         }
         /*
-          Create the schema directories. 
-          Recursive option helps to create a new folder inside a non-existing folder. 
-          It also helps to update only target files in the created directory if the directory already existed. 
+          Create the schema directories.
+          Recursive option helps to create a new folder inside a non-existing folder.
+          It also helps to update only target files in the created directory if the directory already existed.
           Recursive only works for node 10^
         */
         if (workingDirectory) {
@@ -151,14 +115,19 @@ try {
 
         if (verbose === true) {
           console.log('');
-          console.log(`${P_INFO}Generating ${format.toUpperCase()} schema for '${P_END}${P_PATH}${metaSchemaPath}${P_END}' as '${P_PATH}${schemaFile}${P_END}'.`);
+          console.log(`${colorCodes.P_INFO}Generating ${format.toUpperCase()} schema for '${colorCodes.P_END}${colorCodes.P_PATH}${metaSchemaFullPath}${colorCodes.P_END}' as '${colorCodes.P_PATH}${schemaFile}${colorCodes.P_END}'.`);
         }
+
+        // Get all files in the metaschema directory
+        const sourceFiles = fs.readdirSync(`${oscalRootDirectory}/${metaschemaFileRootDir}`);
+        // Get only files with xml extensions in the metaschema directory
+        const getXMLSourceFiles = sourceFiles.filter((file) => file.endsWith('xml'));
 
         // Check if the metaschame defined in the config exists as a file in the metaschema directory (these are the source files)
         const metaSchemaExist = getXMLSourceFiles.includes(metaschemaFile);
 
         if (metaSchemaExist) {
-          // If working directory is defined 
+          // If working directory is defined
           if (workingDirectory) {
             /*
               Create the schema files in the respective XML and JSON directories, relative to the defined working directory.
@@ -166,7 +135,7 @@ try {
             */
             fs.openSync(`${workingDirectory}/${schemaFile}`, 'w');
             // populate the schema files
-            populateSchema(oscalRootDirectory, workingDirectory, format, base, verbose);
+            populateSchema(oscalRootDirectory, workingDirectory, metaschemaFileRootDir, format, base, verbose);
           } else {
             /*
               Create the schema files in the respective XML and JSON directories, using project root directory as default.
@@ -174,15 +143,15 @@ try {
             */
             fs.openSync(`${oscalRootDirectory}/${schemaFile}`, 'w');
             // populate the schema files
-            populateSchema(oscalRootDirectory, '', format, base, verbose);
+            populateSchema(oscalRootDirectory, '', metaschemaFileRootDir, format, base, verbose);
           }
 
           if (verbose === true) {
-            console.log(`${P_OK}Generation of ${format.toUpperCase()} schema passed for '${P_END}${P_PATH}${metaSchemaPath}${P_END}${P_OK}'${P_END}.`);
+            console.log(`${colorCodes.P_OK}Generation of ${format.toUpperCase()} schema passed for '${colorCodes.P_END}${colorCodes.P_PATH}${metaSchemaFullPath}${colorCodes.P_END}${colorCodes.P_OK}'${colorCodes.P_END}.`);
           }
         } else { // If metaschema does not exist
-          console.log(`${P_ERROR}Generation of ${format.toUpperCase()} schema failed for '${P_END}${P_PATH}${metaSchemaPath}${P_END}${P_ERROR}'${P_END}.`);
-          console.log(`${P_ERROR}Metaschema does not exist: ${P_END}${P_PATH}${oscalRootDirectory}/${metaSchemaPath}${P_END}`);
+          console.log(`${colorCodes.P_ERROR}Generation of ${format.toUpperCase()} schema failed for '${colorCodes.P_END}${colorCodes.P_PATH}${metaSchemaFullPath}${colorCodes.P_END}${colorCodes.P_ERROR}'${colorCodes.P_END}.`);
+          console.log(`${colorCodes.P_ERROR}Metaschema does not exist: ${colorCodes.P_END}${colorCodes.P_PATH}${oscalRootDirectory}/${metaSchemaFullPath}${colorCodes.P_END}`);
         }
       });
     }
