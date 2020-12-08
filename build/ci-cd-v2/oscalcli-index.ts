5d89f387601f39c3427e2395a8d8e43f20543c1d
Adougla2 <braddoug76@yahoo.com>
2020-12-07 19:40:57 -0500

    Added code to make the help feature global

--- build/ci-cd-v2/oscalcli-index.ts
+++ build/ci-cd-v2/oscalcli-index.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
-'use strict'
+//'use strict'
 
 import { join } from 'path'
 import yargs from 'yargs'
 
 yargs
     .commandDir(join(__dirname, 'oscal-cli-app', 'lib', 'commands'))
-    .help()
+    .demandCommand()
+   // .help()
+    .alias('help', 'h')
     .argv;
