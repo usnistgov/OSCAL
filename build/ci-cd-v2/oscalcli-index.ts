2744cc358d7fbaa3d431db837c5f4040ddb8adcd
Adougla2 <braddoug76@yahoo.com>
2020-12-16 01:05:47 -0500

    Added .strict method to restrict any unspecified commands or options

--- build/ci-cd-v2/oscalcli-index.ts
+++ build/ci-cd-v2/oscalcli-index.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-//'use strict'
+"use strict";
 
 import { join } from 'path'
 import yargs from 'yargs'
 
 yargs
     .commandDir(join(__dirname, 'oscal-cli-app', 'lib', 'commands'))
-    .demandCommand()
-   // .help()
     .alias('help', 'h')
+    .demandCommand()
+    .strict()
     .argv;
