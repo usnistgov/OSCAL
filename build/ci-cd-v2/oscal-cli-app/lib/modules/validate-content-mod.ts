b245be4737b806ba7352f2df96ead75105fa0a00
Adougla2 <braddoug76@yahoo.com>
2021-04-15 13:54:57 -0400

    Issue 831: Implemented libxmljs2-xsd (XML) and API for ajv (JSON) for the OSCAL content validation

--- build/ci-cd-v2/oscal-cli-app/lib/modules/validate-content-mod.ts
+++ build/ci-cd-v2/oscal-cli-app/lib/modules/validate-content-mod.ts
@@ -1,9 +1,7 @@
-process.cwd();
-
 import fs from 'fs';
 import {join} from 'path';
-import {spawnSync} from 'child_process';
 import Ajv from 'ajv';
+const libxmljs = require('libxmljs2-xsd');
 
 import {colorCodes} from '../utils/init';
 const {P_END, P_INFO, P_ERROR, P_OK, P_PATH, P_WARN} = colorCodes
@@ -18,13 +16,13 @@ export const validateContent = (
         const projectRootDir = join(__dirname, '../../../../..');
         //artifactDir is the directory that contains the models that is to be validated by the schemas
         const artifactRootDir = artifactDir ? join(__dirname, `../../../../../../${artifactDir}`) : projectRootDir;
-        // workingDir is the working directory you passed when generating the schemas
+        //workingDir is the working directory you passed when generating the schemas
         const schemaRootDir = workingDir ? `${projectRootDir}/${workingDir}` : projectRootDir;
 
         console.log('');
         console.log(`${P_INFO}Validating Content${P_END}`);
         console.log(`${P_INFO}==================${P_END}`);
-
+      //If verbose is true, display headers
         if (verbose === true) {
             console.log(`${P_INFO}Using OSCAL directory:${P_END} ${P_PATH}${schemaRootDir}${P_END}`);
             console.log(`${P_INFO}Using artifact directory:${P_END} ${P_PATH}${artifactRootDir}${P_END}`);
@@ -44,12 +42,13 @@ export const validateContent = (
             let filesToProcess: string[];
 
             if(fileFinder) {
-              filesToProcess = fs.readdirSync(`${artifactRootDir}/${pathRoot}`).filter(file => file.endsWith(`${fileFinder}`))
+              filesToProcess = fs.readdirSync(`${artifactRootDir}/${pathRoot}`)
+                .filter(file => file.endsWith(`${fileFinder}`))
                 .map(fileName => `${pathRoot}/${fileName}`)
-            }  else {
-              filesToProcess = [pathRoot];
+            }else{
+                filesToProcess = [pathRoot];
             }  
-      
+            
             filesToProcess.map((fileName) => `${artifactRootDir}/${fileName}`).forEach((file, index) => {
               if (verbose === true) {
                 console.log(`${P_INFO}Validating ${model} ${format} file:${P_END} ${P_PATH}'${filesToProcess[index]}'${P_END}.`);
@@ -67,25 +66,23 @@ export const validateContent = (
                 if (!file) {
                   console.log(`${P_ERROR}The XML file must be provided as the second argument.${P_END}`)
                 }
+                
+                //Uses libxmljs2-xsd for the XML validation
+                const xsdSchemaPath = schema;
+                const xsdSchema = libxmljs.parseFile(xsdSchemaPath)
+                const xmlFileContent = fs.readFileSync(file, 'utf8');
+                const validationErrors = xsdSchema.validate(xmlFileContent);
 
-                const scriptArguments = [
-                  '--noout',
-                  `--schema ${schema}`,
-                  `${file}`,
-                ];
-      
-                const result: any = spawnSync('xmllint', scriptArguments, {shell: true, stdio: 'inherit'});
-      
-                if (!result || result.status !== 0) {
-                  console.log(`${P_ERROR}XML Schema validation failed for '${P_END}${P_PATH}${file}${P_END}${P_PATH} using schema ${schema}${P_END}${P_ERROR}'${P_END}.`);
-      
+                if (validationErrors) {
+                  console.log(`${P_ERROR}XML Schema validation failed for '${P_END}${P_PATH}${filesToProcess[index]}${P_ERROR}' using schema '${P_END}${P_PATH}${schemaFile}${P_ERROR}'.`);
                   process.exit(1);
                 } else {
-                  console.log(`${P_OK}XML Schema validation passed for '${P_END}${P_PATH}${file}${P_END}${P_PATH} using schema ${schema}${P_END}${P_OK}'${P_END}.`);
+                  console.log(`${P_OK}XML Schema validation passed for '${P_END}${P_PATH}${filesToProcess[index]}${P_OK}'${P_END} ${P_OK}using schema '${P_END}${P_PATH}${schemaFile}${P_OK}'.`);
                 }
               }
               //If format is JSON
-              else if (format === 'json') {
+             
+               if (format === 'json') {
                 if (!schema) {
                   console.log(`${P_ERROR}The JSON schema must be provided as the first argument.${P_END}`)
                 }
@@ -95,13 +92,14 @@ export const validateContent = (
                 
                 //Uses AJV for the JSON validation
                 const ajv = new Ajv();
-                const schemaContent = fs.readFileSync(schema, 'utf8');
-                const validate = ajv.compile(JSON.parse(schemaContent));
-                const fileContent = fs.readFileSync(file, 'utf8');
-                const valid = validate(JSON.parse(fileContent));
-
-                if (!valid) {
-                  console.log(`${P_ERROR}JSON Schema validation failed for '${P_END}${P_PATH}${filesToProcess[index]}${P_END}${P_PATH} using schema ${schemaFile}${P_END}${P_ERROR}'.`);
+                const jsonSchema = schema
+                const jsonSchemaContent = fs.readFileSync(jsonSchema, 'utf8');
+                const validate = ajv.compile(JSON.parse(jsonSchemaContent));
+                const jsonContent = fs.readFileSync(file, 'utf8');
+                const valid = validate(JSON.parse(jsonContent));
+            
+                if (!valid){
+                  console.log(`${P_ERROR}JSON Schema validation failed for '${P_END}${P_PATH}${filesToProcess[index]}${P_ERROR}' using schema '${P_END}${P_PATH}${schemaFile}${P_ERROR}'.`);
                   console.log(`${file} is invalid.`);
                   process.exit(1);
                 } else {
