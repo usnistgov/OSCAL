8ced4a2cf572a752c3f427755e3249069b1d244c
David Waltermire <david.waltermire@nist.gov>
2021-12-10 14:17:30 -0500

    fix python calls to use correct python version and script

--- build/ci-cd/copy-and-convert-content.sh
+++ build/ci-cd/copy-and-convert-content.sh
@@ -154,7 +154,7 @@ post_process_content() {
       if [ "$VERBOSE" = "true" ]; then
         echo -e "${P_INFO}Translating relative XML paths to JSON paths in '${P_END}${target_file_relative}${P_INFO}'.${P_END}"
       fi
-      python "$OSCALDIR/build/ci-cd/python/convert-filetypes.py" --old-extension xml --new-extension json "${target_file}"
+      python3 "$OSCALDIR/build/ci-cd/python/convert_filetypes.py" --old-extension xml --new-extension json "${target_file}"
     fi
 
     # produce pretty JSON
@@ -199,7 +199,7 @@ post_process_content() {
     if [ "$VERBOSE" = "true" ]; then
       echo -e "${P_INFO}Translating relative paths in '${P_END}${yaml_file_relative}${P_INFO}'.${P_END}"
     fi
-    python "$OSCALDIR/build/ci-cd/python/convert-filetypes.py" --old-extension json --new-extension yaml "${yaml_file}"
+    python3 "$OSCALDIR/build/ci-cd/python/convert_filetypes.py" --old-extension json --new-extension yaml "${yaml_file}"
     echo -e "${P_OK}Created YAML '${P_END}${yaml_file_relative}${P_OK}'.${P_END}"
     ;;
   xml)
