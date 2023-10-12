5769c5aad2004e69618bc17c0367f5aa343d7381
Nikita Wootten <nikita.wootten@nist.gov>
2023-10-12 16:32:30 -0400

    [WIP] overhaul comparison + results printing

--- src/specifications/profile-resolution/spec-tester.py
+++ src/specifications/profile-resolution/spec-tester.py
@@ -12,9 +12,10 @@ import subprocess
 import tempfile
 import shutil
 import json
+from itertools import zip_longest
 from xml.etree import ElementTree as ET
 
-from typing import TypedDict, List, Dict, Set, Optional
+from typing import TypedDict, List, Dict, Set, Tuple, Optional
 
 
 class Colors:
@@ -129,23 +130,50 @@ class Driver(object):
         shutil.rmtree(self.out_directory)
 
 
-def elements_equal(e1: ET.ElementTree, e2: ET.ElementTree) -> bool:
+def compare_elements(e1: Optional[ET.ElementTree], e2: Optional[ET.ElementTree], path=".", e1Name="left", e2Name="right") -> Tuple[bool, List[str]]:
     """
-    Compare two element trees
+    Compare two element trees, returning if they are the same, and a list of XPath-like selections
 
-    Via https://stackoverflow.com/a/24349916
+    Note: comments added to some difference paths using XPath 2.0 (: comment syntax :)
     """
-    if e1.tag != e2.tag:
-        return False
-    if e1.text != e2.text:
-        return False
-    if e1.tail != e2.tail:
-        return False
-    if e1.attrib != e2.attrib:
-        return False
-    if len(e1) != len(e2):
-        return False
-    return all(elements_equal(c1, c2) for c1, c2 in zip(e1, e2))
+
+    differences: List[str] = []
+
+    if e1 is None:
+        differences.append(f"{path}/ (: selection only present on {e2Name} :)")
+    elif e2 is None:
+        differences.append(f"{path}/ (: selection only present on {e1Name} :)")
+    else:
+        if e1.tag != e2.tag:
+            # Fail early if tags are mismatched, no point in comparing tag contents
+            differences.append(
+                f"{path}/ (: tag mismatch: {e1Name}={e1.tag}, {e2Name}={e2.tag} :)")
+        else:
+            e1Text = (e1.text if e1.text is not None else "").strip()
+            e2Text = (e2.text if e2.text is not None else "").strip()
+
+            if e1Text != e2Text:
+                differences.append(path + "/text()")
+
+            e1AttribSet = set(e1.attrib.keys())
+            e2AttribSet = set(e2.attrib.keys())
+
+            for key in e1AttribSet.intersection(e2AttribSet):
+                if e1.attrib[key] != e2.attrib[key]:
+                    # Attribute value mismatch
+                    differences.append(path + "/@" + key)
+            for key in list(e1AttribSet.difference(e2AttribSet)) + list(e2AttribSet.difference(e1AttribSet)):
+                # Attribute not present in one or the other
+                differences.append(path + "/@" + key)
+
+            for i, (c1, c2) in enumerate(zip_longest(e1, e2)):
+                # zip_longest returns None for extra items of the shorter iterator
+                # XPath starts lists with 1
+                _, child_differences = compare_elements(
+                    c1, c2, path=f"{path}/[{i + 1}]", e1Name=e1Name, e2Name=e2Name)
+                differences += child_differences
+
+    return len(differences) == 0, differences
 
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -156,6 +184,8 @@ QUERY_NS = {
     "specml": "http://csrc.nist.gov/ns/oscal/specml",
     "oscal": "http://csrc.nist.gov/ns/oscal/1.0"
 }
+
+INDENT_TEXT = "  "
 
 
 class RequirementTests(object):
@@ -249,29 +279,49 @@ class RequirementTests(object):
 
         try:
             for test in self.tests:
-                print(
-                    f"Test requirement: {test['section_id']}/{test['requirement_id']}")
-                test_pass = self._run_test(driver, test)
-
-                if not test_pass:
+                test_info = f"requirement({test['section_id']}/{test['requirement_id']})"
+                print(f"{Colors.BOLD}{test_info}{Colors.END}")
+                if self._run_test(driver, test, indent=1):
+                    print(
+                        f"{Colors.BOLD}{Colors.GREEN}{test_info}... PASS{Colors.END}")
+                else:
+                    print(
+                        f"{Colors.BOLD}{Colors.RED}{test_info}... FAIL{Colors.END}")
                     suite_pass = False
         finally:
             if do_cleanup:
                 driver.cleanup()
 
+        if suite_pass:
+            print(
+                f"\n{Colors.GREEN}Spec suite {self.tests_path}... PASS{Colors.END}")
+        else:
+            print(f"\n{Colors.RED}Spec suite {self.tests_path}... FAIL{Colors.END}")
+
         return suite_pass
 
-    def _run_test(self, driver: Driver, requirement: TestRequirement) -> bool:
+    def _run_test(self, driver: Driver, requirement: TestRequirement, indent=0) -> bool:
         test_pass = True
 
         for scenario in requirement["scenarios"]:
-            scenario_pass = self._run_test_scenario(driver, scenario)
-            if not scenario_pass:
+            scenario_info = f"{INDENT_TEXT * indent}scenario(source='{scenario['source_profile_path']}', expected='{scenario['expected_catalog_path']}')"
+
+            print(f"{Colors.BOLD}{scenario_info}{Colors.END}")
+
+            scenario_pass = self._run_test_scenario(
+                driver, scenario, indent=indent + 1)
+
+            if scenario_pass:
+                print(
+                    f"{Colors.BOLD}{Colors.GREEN}{scenario_info}... PASS{Colors.END}")
+            else:
+                print(
+                    f"{Colors.BOLD}{Colors.RED}{scenario_info}... FAIL{Colors.END}")
                 test_pass = False
 
         return test_pass
 
-    def _run_test_scenario(self, driver: Driver, scenario: TestScenario) -> bool:
+    def _run_test_scenario(self, driver: Driver, scenario: TestScenario, indent=0) -> bool:
         """
         Runs a given test scenario, returning True if all selection expressions pass
         """
@@ -293,18 +343,24 @@ class RequirementTests(object):
             expected_selection = expected.findall(
                 selection_expression, QUERY_NS)
 
-            print("results:", result_selection, expected_selection)
-
             for i, (result_elem, expected_elem) in enumerate(zip(result_selection, expected_selection)):
-                if not elements_equal(result_elem, expected_elem):
+                same, differences = compare_elements(
+                    result_elem, expected_elem, f"{selection_expression}[{i}]", e1Name="result", e2Name="expected")
+                if not same:
                     scenario_pass = False
-                    # TODO print both elements for comparison
                     print(
-                        f"    Selection {selection_expression} yielded mismatching results (index={i})")
+                        f"{Colors.RED}{INDENT_TEXT * indent}selection '{selection_expression}' result mismatch (index={i}):{Colors.END}")
+                    for difference in differences:
+                        # Clean up tags in comments to use namespaces
+                        difference = difference.replace(
+                            f"{{{QUERY_NS['oscal']}}}", "oscal:")
+
+                        print(
+                            f"{Colors.RED}{INDENT_TEXT * (indent + 1)}{difference}{Colors.END}")
 
             if len(result_selection) != len(expected_selection):
-                print("Resulting selection lengths do not match")
-                # TODO print extra elements
+                print(
+                    f"{Colors.RED}{INDENT_TEXT * (indent + 1)}selection '{selection_expression}' result size mismatch (got {len(result_selection)}, expecting {len(expected_selection)}){Colors.END}")
                 scenario_pass = False
 
         return scenario_pass
