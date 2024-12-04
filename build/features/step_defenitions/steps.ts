1d3a72c2b89496aa5220c34d096762a3e499385b
~ . ~ <paul.n.wand@gsa.gov>
2024-12-04 09:57:09 -0500

    amend

--- build/features/step_defenitions/steps.ts
+++ build/features/step_defenitions/steps.ts
@@ -1,8 +1,7 @@
 import { Given, Then, When } from "@cucumber/cucumber";
-import { formatSarifOutput, isOscalExecutorInstalled } from 'oscal';
-import { validateDirectory, validateDocument } from "oscal/dist/validate";
-import {statSync} from 'fs'
-import {resolve} from 'path'
+import { statSync } from 'fs';
+import { resolve } from 'path';
+import {execSync} from "child_process"
 Given('the following directories by type:', function(dataTable:any) {
   this.dirsByType = {};
   dataTable.rows().forEach(([type, paths]:any) => {
@@ -11,7 +10,7 @@ Given('the following directories by type:', function(dataTable:any) {
 });
 
 Given('the OSCAL CLI tool is installed', async function() {
-  const success = await isOscalExecutorInstalled("oscal-cli");
+  const success = await execSync("which oscal-cli");
   if (!success) throw new Error('OSCAL CLI not installed');
 });
 
@@ -23,16 +22,10 @@ When('I validate {string} content in {string}',{timeout:90000}, async function(t
 
   const metaschema = 'oscal_'+type+'_metaschema.xml' ;
   const metaschemaPath = `${this.metaschemaDir}/${metaschema}`;
- // Check if path is directory
- const isDirectory = path.includes("http")?false:statSync(path).isDirectory();
- const quiet = false;
- this.result = isDirectory ? 
-   await validateDirectory(path, {module: resolve(metaschemaPath),quiet}, 'oscal-cli') :
-   await validateDocument(path, {module: resolve(metaschemaPath),quiet}, 'oscal-cli');
-});
-Then('all validations should pass without errors', function() {
-  if (!this.result.isValid) {
-    throw new Error(`Validation failed: ${formatSarifOutput(this.result.log).replace(/\u001b\[[0-9;]*m/g, '')}`);
-  }
-});
+  await execSync('oscal-cli metaschema validate-content '+path+"-m "+metaschemaPath);
+  Then('all validations should pass without errors', function() {
+    if (!this.result.isValid) {
+      throw new Error(`Validation failed:`);
+    }
+  });
 
